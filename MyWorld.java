070f79b7cbbfa8e134669e75a8fee7a2c1eeaabf
Victor321-coder <440004873@gapps.yrdsb.ca>
2025-12-09 13:45:33 -0500

    I want to set the dungeon image, but i need to put the dungeon inside the image folder

--- MyWorld.java
+++ MyWorld.java
@@ -4,4 +4,8 @@ public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
     }
+    
+    public void setDungeonImage() {
+        GreenfootImage dungeonImage = new GreenfootImage ("images/dungeon.jpg");
+    }
 }
