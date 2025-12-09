6cf8eee35ad04089404917e9e944af5b7d660538
Victor321-coder <440004873@gapps.yrdsb.ca>
2025-12-09 13:49:58 -0500

    I make it functional

--- MyWorld.java
+++ MyWorld.java
@@ -1,11 +1,10 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    GreenfootImage dungeonImage = new GreenfootImage ("images/dungeon.jpg");
+    
     public MyWorld() {
         super(600, 400, 1);
-    }
-    
-    public void setDungeonImage() {
-        GreenfootImage dungeonImage = new GreenfootImage ("images/dungeon.jpg");
+        setBackground(dungeonImage);
     }
 }
