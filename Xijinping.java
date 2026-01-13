580b61cb2dbdd49edecb6bf9cc5b3beaa1f02ad4
Victor321-coder <440004873@gapps.yrdsb.ca>
2026-01-13 13:45:18 -0500

    Update Xijinping.java

--- Xijinping.java
+++ Xijinping.java
@@ -20,10 +20,6 @@ public class Xijinping extends Actor
     // ===== STATE =====
     String direction = "idle";
     String state = "move";
-    
-    // ===== STATE DURATIONS =====
-    int moveDuration = 4000;   // 4 seconds NOT shooting
-    int shootDuration = 1500; // 1.5 seconds shooting
 
     int speed = 4;
 
@@ -87,22 +83,14 @@ public class Xijinping extends Actor
         if (state.equals("move"))
         {
             moveTowardHero();
-            
-             // stay in move longer
-            if (stateTimer.millisElapsed() > moveDuration)
-            {
-                state = "pause";   // shooting state
-                stateTimer.mark();
-            }
         }
         else
         {
             autoShoot();
-
-            if (stateTimer.millisElapsed() > shootDuration)
+            if (stateTimer.millisElapsed() > 2000)
             {
-                state = "move";
                 stateTimer.mark();
+                state = "move";
             }
         }
     }
@@ -154,8 +142,6 @@ public class Xijinping extends Actor
     // ================= DAMAGE =================
     private void checkHeroAttack()
     {
-        if (!state.equals("move")) return;
-        
         if (getWorld().getObjects(Hero.class).isEmpty()) return;
         Hero hero = getWorld().getObjects(Hero.class).get(0);
 
